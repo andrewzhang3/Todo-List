82db8e6f6b733086058b206e43d44958f2937545
Andrew Zhang <andrew.zhang9799@gmail.com>
2019-08-15 17:10:21 +1000

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI:
+        "mongodb+srv://azhang3:azhang3@shopping-list-n4psa.mongodb.net/test?retryWrites=true&w=majority"
+};
---
+++ routes/api/items.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+
+// Item Model
+const Item = require("../../models/Item");
+
+// @routes  GET api/items
+// @desc    Gets All Items
+// @access  Public
+router.get("/", (req, res) => {
+    Item.find()
+        .sort({ date: -1 })
+        .then(items => res.json(items));
+});
+
+// @routes  POST api/items
+// @desc    Create A Item
+// @access  Public
+router.post("/", (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+
+    newItem
+        .save()
+        .then(item => res.json(item))
+        .catch(err => Error(err));
+});
+
+// @routes  DELETE api/items/:id
+// @desc    Delete A Item
+// @access  Public
+router.delete("/:id", (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({ success: true })))
+        .catch(err => res.status(404).json({ success: false }));
+});
+
+module.exports = router;
