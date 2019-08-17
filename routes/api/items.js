1ebff9fa32edff2d118a15043f1d3cc961e02fc0
Andrew Zhang <andrew.zhang9799@gmail.com>
2019-08-18 00:38:32 +1000

    initial commit

--- routes/api/items.js
+++ routes/api/items.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Item Model
-const Item = require("../../models/Item");
+const Item = require("../../models/item");
 
 // @routes  GET api/items
 // @desc    Gets All Items
